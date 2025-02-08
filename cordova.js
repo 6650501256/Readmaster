2e8c40347f4f269345ce6a4cec6a1863fc1fa58d
ReadMaster <darunpon.p@kkumail.com>
2025-02-08 19:51:48 +0700

    Update cordova.js

--- cordova.js
+++ cordova.js
@@ -1,6 +1,1 @@
-/*!
-This file is part of DecSoft App Builder.
-Visit our website for license information.
-Copyright © DecSoft Utils - All rights reserved.
-Visit our website at: https://www.decsoftutils.com
-*/
+
